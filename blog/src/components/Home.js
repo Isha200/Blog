bdd5bd8ce98939b7cbbcc904396708451e862589
ishupadh1 <you@example.comishupadh1@publicisgroupe.net>
2023-02-01 10:59:11 +0000

    useState

--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -1,12 +1,22 @@
+import {useState} from 'react';  
+
 const Home = () => {
+
+  // let name = 'Isha'; //not reactive
+
+  const [name, setName] = useState('Isha');
+
   const handleClick = () => {
-    console.log("Hello");
+    setName('tom')
   };
+
 
   return (
     <div className="home">
       <h2>Homepage</h2> 
+      <p>{name}</p>
       <button onClick={handleClick}>Click Me!</button>
+      
     </div>
   );
 };
