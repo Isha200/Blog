efb843ad8184936371aa71f39d293db92d741600
ishupadh1 <you@example.comishupadh1@publicisgroupe.net>
2023-02-08 14:39:39 +0000

    delete blog

--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -1,24 +1,23 @@
-import {useState} from 'react';  
+import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
-const [blogs,setBlogs] = useState([
-  //list using useState
-  {title: "My new website", body:"blah blah blah", author:"Isha", id:1},
-  {title: "food blog", body:"blah blah blah", author:"Ayesha", id:2},
-  {title: "Make up blog", body:"blah blah blah", author:"Chris", id:3},
-])
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return (
     <div className="home">
-        {blogs.map((blog) => (
-          //key id for unique property
-          <div className="blog-preview" key={blog.id}>
-            <h2>{blog.title}</h2>
-            <p>Written by {blog.author}</p>
-          </div>
-        ))}
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
-};
-
+}
+ 
 export default Home;
